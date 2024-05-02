ea0b51f7f0fc05f337f5c5e6e7f404b542629b7e
Berndy <al-berndt@gmx.de>
2024-05-02 20:12:55 +0200

    selectSuccessAndErrorGraphs

--- packages/got-core/src/graph.util.ts
+++ packages/got-core/src/graph.util.ts
@@ -1,5 +1,12 @@
-import { Edges, Files, Graph, NodeFilesView, Nodes, Rights } from './types/graph';
-import { Metadata, NodeFileView, NodeRightsView } from './graphObjects';
+import { Edges, ErrorGraph, Files, Graph, NodeFilesView, Nodes, PushResult, Rights } from './types/graph';
+import {
+    GraphElementResult,
+    GraphError,
+    Metadata,
+    NodeFileView,
+    NodeRightsView,
+    UploadNodeFileView,
+} from './graphObjects';
 import { State } from './types/state';
 import {
     assocPathMutate,
@@ -426,4 +433,81 @@ export const selectSubGraph = (graphStack: Graph[], view: View, state: State): G
         rights,
         files,
     };
+};
+
+export const selectSuccessAndErrorGraphs = (pushGraph: Graph, pushResult: PushResult): [Graph, ErrorGraph] => {
+    const successNodes = {} as Nodes<Node | boolean>;
+    const successEdges = {} as Edges<Metadata>;
+    const successRights = {} as Rights<boolean, string>;
+    const successFiles = {} as Files<NodeFileView>;
+
+    const errorNodes = {} as Nodes<GraphError<Node | boolean>>;
+    const errorEdges = {} as Edges<GraphError<Metadata>>;
+    const errorRights = {} as Rights<GraphError<boolean>, GraphError<string>>;
+    const errorFiles = {} as Files<GraphError<UploadNodeFileView>>;
+
+    forEachObjDepth(
+        pushResult.nodes,
+        (res: GraphElementResult, path: string[]) => {
+            const node = getPathOr(undefined, path, pushGraph);
+            if (res.statusCode === 200) {
+                assocPathMutate(path, node, successNodes);
+            } else {
+                assocPathMutate(path, { ...res, element: node }, errorNodes);
+            }
+        },
+        1,
+    );
+    forEachObjDepth(
+        pushResult.edges,
+        (res: GraphElementResult, path: string[]) => {
+            const metedata = getPathOr(undefined, path, pushGraph);
+            if (res.statusCode === 200) {
+                assocPathMutate(path, metedata, successEdges);
+            } else {
+                assocPathMutate(path, { ...res, element: metedata }, errorEdges);
+            }
+        },
+        4,
+    );
+    forEachObjDepth(
+        pushResult.rights,
+        (res: GraphElementResult, path: string[]) => {
+            const right = getPathOr(undefined, path, pushGraph);
+            if (res.statusCode === 200) {
+                assocPathMutate(path, right, successRights);
+            } else {
+                assocPathMutate(path, { ...res, element: right }, errorRights);
+            }
+        },
+        4,
+    );
+    forEachObjDepth(
+        pushResult.files,
+        (res: GraphElementResult, path: string[]) => {
+            const file = getPathOr(undefined, path, pushGraph);
+            if (res.statusCode === 200) {
+                assocPathMutate(path, file, successFiles);
+            } else {
+                assocPathMutate(path, { ...res, element: file }, errorFiles);
+            }
+        },
+        2,
+    );
+
+    const successGraph = {
+        nodes: successNodes,
+        edges: successEdges,
+        rights: successRights,
+        files: successFiles,
+    };
+
+    const errorGraph = {
+        nodes: errorNodes,
+        edges: errorEdges,
+        rights: errorRights,
+        files: errorFiles,
+    };
+
+    return [successGraph, errorGraph];
 };
