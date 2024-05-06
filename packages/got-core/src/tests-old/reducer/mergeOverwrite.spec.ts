b2b38324503042ac2fd19285d942473fbf1723f8
Berndy <al-berndt@gmx.de>
2024-05-06 22:20:20 +0200

    added mergeOverwrite tests

---
+++ packages/got-core/src/tests-old/reducer/mergeOverwrite.spec.ts
@@ -0,0 +1,87 @@
+import { describe, expect, test } from 'bun:test';
+import { gotReducer } from '../../reducer/reducer';
+
+describe('reducer::mergeOverwrite', () => {
+    test('should merge nodes into existing graph', () => {
+        const graph1 = {
+            graph: {
+                nodes: {
+                    node1: { id: 'node1' },
+                },
+            },
+        };
+
+        const graph2 = {
+            nodes: {
+                node2: { id: 'node2' },
+            },
+        };
+
+        const action = {
+            type: 'GOT/MERGE_OVERWRITE',
+            payload: {
+                fromGraph: graph2,
+                toGraphName: 'graph1',
+            },
+        };
+
+        const result = gotReducer({ graph1 }, action);
+
+        const expectedResult = {
+            graph: {
+                nodes: {
+                    node1: { id: 'node1' },
+                    node2: { id: 'node2' },
+                },
+            },
+        };
+
+        expect(result.graph1).toEqual(expectedResult);
+    });
+    test('should merge correctly to non existing graph', () => {
+        const graph2 = {
+            nodes: {
+                node2: { id: 'node2' },
+            },
+        };
+
+        const action = {
+            type: 'GOT/MERGE_OVERWRITE',
+            payload: {
+                fromGraph: graph2,
+                toGraphName: 'graph1',
+            },
+        };
+
+        const result = gotReducer({}, action);
+
+        const expectedResult = {
+            graph: {
+                nodes: {
+                    node2: { id: 'node2' },
+                },
+            },
+        };
+
+        expect(result.graph1).toEqual(expectedResult);
+    });
+    test('should merge correctly when merging empty object to non existing graph', () => {
+        const graph2 = {};
+
+        const action = {
+            type: 'GOT/MERGE_OVERWRITE',
+            payload: {
+                fromGraph: graph2,
+                toGraphName: 'graph1',
+            },
+        };
+
+        const result = gotReducer({}, action);
+
+        const expectedResult = {
+            graph: {},
+        };
+
+        expect(result.graph1).toEqual(expectedResult);
+    });
+});
