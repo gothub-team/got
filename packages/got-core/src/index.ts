1a729613e50768ad26beb189143c3628b3b6fc3d
Berndy <al-berndt@gmx.de>
2024-05-07 20:15:46 +0200

    first draft of got-core setup

---
+++ packages/got-core/src/index.ts
@@ -0,0 +1,42 @@
+import { configureCreateCurriedGraph } from './store/createCurriedGraph';
+import { configureCreateGraph } from './store/createGraph';
+import { createCurriedStore } from './store/curried';
+import { createErrorHandledStore } from './store/errorHandling';
+import { type GotApi } from '@gothub-team/got-api';
+import { StoreAPI } from './types/api';
+import { State } from './types/state';
+import { GOT_ACTION } from './types/actions';
+
+type SetupOption = {
+    api: GotApi;
+    select: <TRes>(fnSelect: (state: State) => TRes) => TRes;
+    dispatch: (action: GOT_ACTION) => void;
+    onError: (e: Error) => void;
+    onWarn: (e: Error) => void;
+    adminMode?: boolean;
+    sessionExpireTime?: number;
+};
+
+export const setup = ({ api, dispatch, select, onError, onWarn }: SetupOption) => {
+    const store = createErrorHandledStore({
+        dispatch,
+        select,
+        api: api as StoreAPI,
+        onError,
+        onWarn,
+    });
+
+    const createGraph = configureCreateGraph(store, onError);
+
+    const curriedStore = createCurriedStore(store);
+    const createCurriedGraph = configureCreateCurriedGraph(curriedStore, onError);
+
+    return {
+        createGraph,
+        store,
+        curried: {
+            createGraph: createCurriedGraph,
+            store: curriedStore,
+        },
+    };
+};
