77c51d96299340783ec473b565dc9000b2c31d2e
Vincenz Koop <vincenz.koop@gmail.com>
2024-05-11 09:36:13 +0200

    api-test: metadata pull

---
+++ packages/got-api-test/src/metadata.spec.ts
@@ -0,0 +1,85 @@
+import { describe, beforeAll, beforeEach, it, expect } from 'bun:test';
+import { type GotApi } from '@gothub/got-api';
+import crypto from 'crypto';
+import type { Graph, PushResult } from '@gothub-team/got-core';
+import { createUserApi } from './shared';
+import { env } from '../env';
+
+let testId: string;
+let user1Api: GotApi;
+let user1Email: string;
+beforeAll(async () => {
+    user1Email = env.TEST_USER_1_EMAIL;
+    user1Api = await createUserApi(user1Email, env.TEST_USER_1_PW);
+});
+beforeEach(async () => {
+    testId = `test-${crypto.randomBytes(8).toString('hex')}`;
+});
+
+describe('metadata', () => {
+    let pushResult: PushResult;
+    let graph: Graph;
+    beforeEach(async () => {
+        await user1Api.push({
+            nodes: {
+                [`${testId}-1`]: { id: `${testId}-1` },
+                [`${testId}-2`]: { id: `${testId}-2` },
+            },
+        });
+    });
+
+    describe('no metadata exist', () => {
+        beforeEach(async () => {
+            await user1Api.push({
+                edges: {
+                    from1: { [`${testId}-1`]: { to1: { [`${testId}-2`]: true } } },
+                },
+            });
+            graph = await user1Api.pull({
+                [`${testId}-1`]: { edges: { 'from1/to1': { include: { edges: true, metadata: true } } } },
+            });
+        });
+
+        it('pulls only true', () => {
+            expect(graph).toHaveProperty(['edges', 'from1', `${testId}-1`, 'to1', `${testId}-2`], true);
+        });
+    });
+
+    describe('metadata exist', () => {
+        beforeEach(async () => {
+            pushResult = await user1Api.push({
+                edges: {
+                    from1: {
+                        [`${testId}-1`]: {
+                            to1: {
+                                [`${testId}-2`]: {
+                                    strProp: 'some stuff',
+                                    numProp: 42,
+                                    boolProp: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+            graph = await user1Api.pull({
+                [`${testId}-1`]: { edges: { 'from1/to1': { include: { edges: true, metadata: true } } } },
+            });
+        });
+
+        it('pushes edge metadata', () => {
+            expect(pushResult).toHaveProperty(
+                ['edges', 'from1', `${testId}-1`, 'to1', `${testId}-2`, 'statusCode'],
+                200,
+            );
+        });
+        it('pulls edge metadata', () => {
+            expect(graph).toHaveProperty(
+                ['edges', 'from1', `${testId}-1`, 'to1', `${testId}-2`, 'strProp'],
+                'some stuff',
+            );
+            expect(graph).toHaveProperty(['edges', 'from1', `${testId}-1`, 'to1', `${testId}-2`, 'numProp'], 42);
+            expect(graph).toHaveProperty(['edges', 'from1', `${testId}-1`, 'to1', `${testId}-2`, 'boolProp'], true);
+        });
+    });
+});
