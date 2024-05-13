68f4ab2b687fabf53d4035f0dfe555ed0a085c44
Vincenz Koop <vincenz.koop@gmail.com>
2024-05-13 16:37:43 +0200

    assoc path mutate edge case

--- packages/got-core/src/index.ts
+++ packages/got-core/src/index.ts
@@ -52,3 +52,4 @@ export const setup = ({ api, dispatch, select, onError, onWarn }: SetupOption) =
 export type * from './types/graph';
 export type * from './types/graphObjects';
 export type * from './types/view';
+export * from './utils/util';
