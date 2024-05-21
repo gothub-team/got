4334e2ce19ac263ece39b57677b9e0483ba23d03
Berndy <al-berndt@gmx.de>
2024-05-21 15:40:15 +0200

    test api deployment settings

--- packages/got-api/sst.config.ts
+++ packages/got-api/sst.config.ts
@@ -19,11 +19,24 @@ export default $config({
         };
     },
     async run() {
-        const lambda = new gotiac.Lambda('TestLambda', {
+        // const lambda = new gotiac.Lambda('TestLambda', {
+        //     runtime: 'nodejs18.x',
+        //     codePath: path.join(process.cwd(), 'index.zip'),
+        //     handlerPath: 'index.handler',
+        //     policyArns: [],
+        // });
+
+        const api = new gotiac.Api('TestApi', {
+            userPoolId: 'eu-central-1_orSPNPiED',
+            routePath: '/test',
             runtime: 'nodejs18.x',
             codePath: path.join(process.cwd(), 'index.zip'),
             handlerPath: 'index.handler',
             policyArns: [],
         });
+
+        return {
+            apiEndpoint: api.endpoint,
+        };
     },
 });
