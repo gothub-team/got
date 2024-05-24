f1ecf6b4a0efb2f88263c951b88f6bb83b96f73a
Berndy <al-berndt@gmx.de>
2024-05-24 19:04:21 +0200

    added graphstore to sst config

--- packages/got-api/sst.config.ts
+++ packages/got-api/sst.config.ts
@@ -3,6 +3,9 @@
 import * as gotiac from '@gothub/pulumi-gotiac-aws';
 import * as path from 'path';
 
+const AWS_REGION = '';
+const AWS_PROFILE = '';
+
 export default $config({
     app(input) {
         return {
@@ -12,8 +15,8 @@ export default $config({
             providers: {
                 aws: {
                     // TODO: Add AWS env
-                    region: '',
-                    profile: '',
+                    region: AWS_REGION,
+                    profile: AWS_PROFILE,
                 },
             },
         };
@@ -25,7 +28,6 @@ export default $config({
         //     handlerPath: 'index.handler',
         //     policyArns: [],
         // });
-
         const api = new gotiac.Api('TestApi', {
             userPoolId: 'eu-central-1_orSPNPiED',
             routePath: '/test',
@@ -34,9 +36,26 @@ export default $config({
             handlerPath: 'index.handler',
             policyArns: [],
         });
-
+        const graphStorage = new gotiac.GraphStore('TestGraphStore', {
+            bucketNodesName: `${AWS_PROFILE}-nodes`,
+            bucketEdgesName: `${AWS_PROFILE}-edges`,
+            bucketReverseEdgesName: `${AWS_PROFILE}-reverse-edges`,
+            bucketRightsReadName: `${AWS_PROFILE}-rights-read`,
+            bucketRightsWriteName: `${AWS_PROFILE}-rights-write`,
+            bucketRightsAdminName: `${AWS_PROFILE}-rights-admin`,
+            bucketRightsOwnerName: `${AWS_PROFILE}-owners`,
+        });
         return {
-            apiEndpoint: api.endpoint,
+            nodes: graphStorage.bucketNodes,
+            nodesName: graphStorage.bucketNodes.apply((a) => a.name),
+            edges: graphStorage.bucketEdges,
+            reverseEdges: graphStorage.bucketReverseEdges,
+            rightsRead: graphStorage.bucketRightsRead,
+            rightsWrite: graphStorage.bucketRightsWrite,
+            rightsAdmin: graphStorage.bucketRightsAdmin,
+            rightsOwner: graphStorage.bucketRightsOwner,
+            readPolicyArn: graphStorage.storageReadPolicyArn,
+            writePolicyArn: graphStorage.storageWritePolicyArn,
         };
     },
 });
