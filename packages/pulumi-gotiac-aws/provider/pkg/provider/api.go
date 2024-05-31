624dccd882e288c1bceccd179ae62fd10bd13aab
Berndy <al-berndt@gmx.de>
2024-05-31 20:36:50 +0200

    auth lambdas (but locked by auth atm)

--- packages/pulumi-gotiac-aws/provider/pkg/provider/api.go
+++ packages/pulumi-gotiac-aws/provider/pkg/provider/api.go
@@ -40,6 +40,15 @@ type Api struct {
 	PushEndpoint pulumi.StringOutput `pulumi:"pushEndpoint"`
 	PullInvokePolicyArn pulumi.StringOutput `pulumi:"pullInvokePolicyArn"`
 	PushInvokePolicyArn pulumi.StringOutput `pulumi:"pushInvokePolicyArn"`
+	AuthLoginInitEndpoint pulumi.StringOutput `pulumi:"authLoginInitEndpoint"`
+	AuthLoginVerifyEndpoint pulumi.StringOutput `pulumi:"authLoginVerifyEndpoint"`
+	AuthLoginRefreshEndpoint pulumi.StringOutput `pulumi:"authLoginRefreshEndpoint"`
+	AuthRegisterInitEndpoint pulumi.StringOutput `pulumi:"authRegisterInitEndpoint"`
+	AuthRegisterVerifyEndpoint pulumi.StringOutput `pulumi:"authRegisterVerifyEndpoint"`
+	AuthRegisterVerifyResendEndpoint pulumi.StringOutput `pulumi:"authRegisterVerifyResendEndpoint"`
+	AuthResetPasswordInitEndpoint pulumi.StringOutput `pulumi:"authResetPasswordInitEndpoint"`
+	AuthResetPasswordVerifyEndpoint pulumi.StringOutput `pulumi:"authResetPasswordVerifyEndpoint"`
+	AuthInviteUserEndpoint pulumi.StringOutput `pulumi:"authInviteUserEndpoint"`
 }
 
 // NewApi creates a new Lambda component resource.
@@ -98,22 +107,16 @@ func NewApi(ctx *pulumi.Context,
 		return nil, err
 	}
 
-	userPool, err := cognito.GetUserPool(ctx, name, args.UserPoolId, &cognito.UserPoolState{});
-	if err != nil {
-		return nil, err
-	}
-
-	userPoolClient, err := cognito.NewUserPoolClient(ctx, "api-client", &cognito.UserPoolClientArgs{
-		Name:           pulumi.String("client"),
-		UserPoolId:     userPool.ID(),
-		GenerateSecret: pulumi.Bool(true),
-		ExplicitAuthFlows: pulumi.StringArray{
-			pulumi.String("ADMIN_NO_SRP_AUTH"),
-		},
+	auth, err := NewUserpool(ctx, name + "-userpool", &UserpoolArgs{
+		UserPoolId: args.UserPoolId,
 	})
 	if err != nil {
 		return nil, err
 	}
+
+	auth.UserPoolClient.ApplyT(func(client *cognito.UserPoolClient) (pulumi.IDOutput) {
+		return client.ID()
+	})
 
 	// Create Cognito Authorizer
 	authorizer, err := apigatewayv2.NewAuthorizer(ctx, fmt.Sprintf("%s-Authorizer", name), &apigatewayv2.AuthorizerArgs{
@@ -123,14 +126,10 @@ func NewApi(ctx *pulumi.Context,
 			pulumi.String("$request.header.Authorization"),
 		},
 		JwtConfiguration: &apigatewayv2.AuthorizerJwtConfigurationArgs{
-			Audiences: userPoolClient.ID().ApplyT(func(id string) (pulumi.StringArray) {
-				return pulumi.StringArray{
-					pulumi.String(id),
-				}
-			}).(pulumi.StringArrayOutput),
-			Issuer: userPool.Endpoint.ApplyT(func(endpoint string) (string) {
-				return "https://" + endpoint
-			}).(pulumi.StringOutput).ToStringPtrOutput(),
+			Audiences: pulumi.StringArray{
+				auth.UserPoolId,
+			},
+			Issuer: pulumi.Sprintf("https://%s", auth.UserPoolEndpoint),
 		},
 	})
 	if err != nil {
@@ -284,6 +283,184 @@ func NewApi(ctx *pulumi.Context,
 		return nil, err
 	}
 
+	AuthMem := pulumi.Int(512)
+	AuthEnv := pulumi.StringMap{
+		"USER_POOL_ID": auth.UserPoolId,
+		"CLIENT_ID": auth.UserPoolClientId,
+	}
+
+	AuthLoginInitApiLambda, err := NewApiLambda(ctx, name + "AuthLoginInit", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/login-init.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/login-init"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	AuthLoginVerifyApiLambda, err := NewApiLambda(ctx, name + "AuthLoginVerify", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/login-verify.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/login-verify"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	AuthLoginRefreshApiLambda, err := NewApiLambda(ctx, name + "AuthLoginRefresh", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/login-refresh.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/login-refresh"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	AuthRegisterInitApiLambda, err := NewApiLambda(ctx, name + "AuthRegisterInit", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/register-init.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/register-init"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	AuthRegisterVerifyApiLambda, err := NewApiLambda(ctx, name + "AuthRegisterVerify", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/register-verify.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/register-verify"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	AuthRegisterVerifyResendApiLambda, err := NewApiLambda(ctx, name + "AuthRegisterVerifyResend", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/register-verify-resend.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/register-verify-resend"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	AuthResetPasswordInitApiLambda, err := NewApiLambda(ctx, name + "AuthResetPasswordInit", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/reset-password-init.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/reset-password-init"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	AuthResetPasswordVerifyApiLambda, err := NewApiLambda(ctx, name + "AuthResetPasswordVerify", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/reset-password-verify.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/reset-password-verify"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	// TODO this needs auth and higher memory maybe?
+	AuthInviteUserApiLambda, err := NewApiLambda(ctx, name + "AuthInviteUser", &ApiLambdaArgs{
+		Runtime: args.Runtime,
+		CodePath: pulumi.Sprintf("%s/auth/invite-user.zip", args.CodePath),
+		HandlerPath: pulumi.String("index.handleHttp"),
+		MemorySize: &AuthMem,
+		Method: pulumi.String("POST"),
+		AuthorizerId: authorizer.ID(),
+		PolicyArns: pulumi.StringArray{
+			auth.AuthUserPolicyArn,
+		},
+		ApiId: api.ID(),
+		ExecutionArn: api.ExecutionArn,
+		RoutePath: pulumi.String("/auth/invite-user"),
+		Environment: AuthEnv,
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	component.Endpoint = stage.InvokeUrl
 	component.PullFunction = pullLambda.Function
 	component.PullEndpoint = pullApiLambda.Route.RouteKey()
@@ -291,6 +468,15 @@ func NewApi(ctx *pulumi.Context,
 	component.PushEndpoint = pushApiLambda.Route.RouteKey()
 	component.PullInvokePolicyArn = pullLambdaInvokePolicy.Arn
 	component.PushInvokePolicyArn = pushLambdaInvokePolicy.Arn
+	component.AuthLoginInitEndpoint = AuthLoginInitApiLambda.Route.RouteKey()
+	component.AuthLoginVerifyEndpoint = AuthLoginVerifyApiLambda.Route.RouteKey()
+	component.AuthLoginRefreshEndpoint = AuthLoginRefreshApiLambda.Route.RouteKey()
+	component.AuthRegisterInitEndpoint = AuthRegisterInitApiLambda.Route.RouteKey()
+	component.AuthRegisterVerifyEndpoint = AuthRegisterVerifyApiLambda.Route.RouteKey()
+	component.AuthRegisterVerifyResendEndpoint = AuthRegisterVerifyResendApiLambda.Route.RouteKey()
+	component.AuthResetPasswordInitEndpoint = AuthResetPasswordInitApiLambda.Route.RouteKey()
+	component.AuthResetPasswordVerifyEndpoint = AuthResetPasswordVerifyApiLambda.Route.RouteKey()
+	component.AuthInviteUserEndpoint = AuthInviteUserApiLambda.Route.RouteKey()
 
 	if err := ctx.RegisterResourceOutputs(component, pulumi.Map{
 		"endpoint": stage.InvokeUrl,
---
+++ packages/pulumi-gotiac-aws/provider/pkg/provider/userpool.go
@@ -0,0 +1,130 @@
+package provider
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/cognito"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+// The set of arguments for creating a Userpool component resource.
+type UserpoolArgs struct {
+	UserPoolId pulumi.IDInput `pulumi:"userPoolId"`
+}
+
+// The Userpool component resource.
+type Userpool struct {
+	pulumi.ResourceState
+	UserPool cognito.UserPoolOutput `pulumi:"userPool"`
+	UserPoolId pulumi.StringOutput `pulumi:"userPoolId"`
+	UserPoolEndpoint pulumi.StringOutput `pulumi:"userPoolEndpoint"`
+	UserPoolClient cognito.UserPoolClientOutput `pulumi:"userPoolClient"`
+	UserPoolClientId pulumi.StringOutput `pulumi:"userPoolClientId"`
+	AuthUserPolicyArn pulumi.StringOutput `pulumi:"authUserPolicyArn"`
+	AuthAdminPolicyArn pulumi.StringOutput `pulumi:"authAdminPolicyArn"`
+}
+
+// NewUserpool creates a new Userpool component resource.
+func NewUserpool(ctx *pulumi.Context,
+	name string, args *UserpoolArgs, opts ...pulumi.ResourceOption) (*Userpool, error) {
+	if args == nil {
+		args = &UserpoolArgs{}
+	}
+
+	component := &Userpool{}
+	err := ctx.RegisterComponentResource("gotiac:index:Userpool", name, component, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	userPool, err := cognito.GetUserPool(ctx, name, args.UserPoolId, &cognito.UserPoolState{});
+	if err != nil {
+		return nil, err
+	}
+
+	userPoolClient, err := cognito.NewUserPoolClient(ctx, name + "-userpoolclient", &cognito.UserPoolClientArgs{
+		Name:           pulumi.String("client"),
+		UserPoolId:     userPool.ID(),
+		GenerateSecret: pulumi.Bool(true),
+		ExplicitAuthFlows: pulumi.StringArray{
+			pulumi.String("ADMIN_NO_SRP_AUTH"),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+ 
+	authUserPolicy, err := iam.NewPolicy(ctx, name + "-auth-user-policy", &iam.PolicyArgs{
+		Path:        pulumi.String("/"),
+		Description: pulumi.String("IAM policy for writing the got s3 storage"),
+		Policy:      pulumi.Any(map[string]interface{}{
+			"Version": "2012-10-17",
+			"Statement": []map[string]interface{}{
+				{
+					"Effect": "Allow",
+					"Action": []interface{}{
+						"cognito-idp:InitiateAuth",
+						"cognito-idp:RespondToAuthChallenge",
+						"cognito-idp:SignUp",
+						"cognito-idp:ConfirmSignUp",
+						"cognito-idp:ResendConfirmationCode",
+						"cognito-idp:ForgotPassword",
+						"cognito-idp:ConfirmForgotPassword",
+					},
+					"Resource": []interface{}{
+						userPool.Arn,
+					},
+				},
+			},
+		}),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	authAdminPolicy, err := iam.NewPolicy(ctx, name + "-auth-admin-policy", &iam.PolicyArgs{
+		Path:        pulumi.String("/"),
+		Description: pulumi.String("IAM policy for writing the got s3 storage"),
+		Policy:      pulumi.Any(map[string]interface{}{
+			"Version": "2012-10-17",
+			"Statement": []map[string]interface{}{
+				{
+					"Effect": "Allow",
+					"Action": []interface{}{
+						"cognito-idp:AdminGetUser",
+						"cognito-idp:AdminCreateUser",
+						"cognito-idp:AdminDeleteUser",
+						"cognito-idp:AdminInitiateAuth",
+						"cognito-idp:AdminRespondToAuthChallenge",
+						"cognito-idp:AdminUpdateUserAttributes",
+						"cognito-idp:AdminSetUserPassword",
+					},
+					"Resource": []interface{}{
+						userPool.Arn,
+					},
+				},
+			},
+		}),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	component.UserPool = userPool.ToUserPoolOutput()
+	component.UserPoolId = userPool.ID().ApplyT(func(id string) (pulumi.String) {
+		return pulumi.String(id)
+	}).(pulumi.StringOutput)
+	component.UserPoolEndpoint = userPool.Endpoint
+	component.UserPoolClient = userPoolClient.ToUserPoolClientOutput()
+	component.UserPoolClientId = userPoolClient.ID().ApplyT(func(id string) (pulumi.String) {
+		return pulumi.String(id)
+	}).(pulumi.StringOutput)
+	component.AuthUserPolicyArn = authUserPolicy.Arn
+	component.AuthAdminPolicyArn = authAdminPolicy.Arn
+
+	if err := ctx.RegisterResourceOutputs(component, pulumi.Map{
+	}); err != nil {
+		return nil, err
+	}
+
+	return component, nil
+}
