81a3832fbec98cfd2286e01882179a96cd770b12
Vincenz Koop <vincenz.koop@gmail.com>
2024-05-31 19:24:15 +0200

    bunx script

--- packages/got-api-test/index.ts
+++ packages/got-api-test/index.ts
@@ -3,6 +3,7 @@
 import { $, argv } from 'bun';
 
 console.log('DIRNAME:', __dirname);
+console.log('VERSION', '0.0.18');
 try {
     await $`cd ${__dirname}; bun test --timeout 100000 ${argv[2]}`;
 } catch (error) {
