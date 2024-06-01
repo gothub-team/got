0368204314371de087144ada954440eb835117ed
Vincenz Koop <vincenz.koop@gmail.com>
2024-06-01 14:29:04 +0200

    new package versions

--- packages/got-api-test/test.ts
+++ packages/got-api-test/test.ts
@@ -2,10 +2,8 @@
 
 import { $, argv } from 'bun';
 
-console.log('DIRNAME:', __dirname);
-console.log('VERSION', '0.0.18');
 try {
-    await $`cd ${__dirname}; bun test --timeout 100000 ${argv[2]}`;
+    await $`cd ${__dirname}; bun test --timeout 100000 ${argv[2] || ''}`;
 } catch (error) {
     //
 }
