775fe8b02471e98c64deead66f467dddac824acb
Berndy <al-berndt@gmx.de>
2024-06-03 11:10:48 +0200

    env for got-api

--- packages/got-api/sst.config.ts
+++ packages/got-api/sst.config.ts
@@ -2,9 +2,13 @@
 
 import * as gotiac from '@gothub/pulumi-gotiac-aws';
 import * as path from 'path';
+import { AWS_PROFILE, AWS_REGION, USER_POOL_ID, parseEnv } from '@gothub/typescript-util';
 
-const AWS_REGION = '';
-const AWS_PROFILE = '';
+const env = parseEnv({
+    AWS_PROFILE,
+    AWS_REGION,
+    USER_POOL_ID,
+});
 
 export default $config({
     app(input) {
@@ -15,24 +19,24 @@ export default $config({
             providers: {
                 aws: {
                     // TODO: Add AWS env
-                    region: AWS_REGION,
-                    profile: AWS_PROFILE,
+                    region: env.AWS_REGION,
+                    profile: env.AWS_PROFILE,
                 },
             },
         };
     },
     async run() {
         const api = new gotiac.Api('TestApi', {
-            userPoolId: 'eu-central-1_orSPNPiED',
+            userPoolId: env.USER_POOL_ID,
             runtime: 'nodejs20.x',
             codePath: path.join(process.cwd(), 'dist/lambda/zips'),
-            bucketNodesName: `${AWS_PROFILE}-nodes`,
-            bucketEdgesName: `${AWS_PROFILE}-edges`,
-            bucketReverseEdgesName: `${AWS_PROFILE}-reverse-edges`,
-            bucketRightsReadName: `${AWS_PROFILE}-rights-read`,
-            bucketRightsWriteName: `${AWS_PROFILE}-rights-write`,
-            bucketRightsAdminName: `${AWS_PROFILE}-rights-admin`,
-            bucketRightsOwnerName: `${AWS_PROFILE}-owners`,
+            bucketNodesName: `${env.AWS_PROFILE}-nodes`,
+            bucketEdgesName: `${env.AWS_PROFILE}-edges`,
+            bucketReverseEdgesName: `${env.AWS_PROFILE}-reverse-edges`,
+            bucketRightsReadName: `${env.AWS_PROFILE}-rights-read`,
+            bucketRightsWriteName: `${env.AWS_PROFILE}-rights-write`,
+            bucketRightsAdminName: `${env.AWS_PROFILE}-rights-admin`,
+            bucketRightsOwnerName: `${env.AWS_PROFILE}-owners`,
         });
         return api;
     },
