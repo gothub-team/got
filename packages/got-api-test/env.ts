e96d742b8f2fc0f6369d570e83cffd1721baa7c7
Vincenz Koop <vincenz.koop@gmail.com>
2024-06-06 21:16:48 +0200

    added admin api

--- packages/got-api-test/env.ts
+++ packages/got-api-test/env.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
 
+export const INVITE_USER_ROOT = z
+    .string()
+    .optional()
+    .default('root')
+    .describe('Root node ID for which the user needs read rights in order to invite other users.');
 export const MAIL_USERNAME = z
     .string()
     .describe('IMAP mailbox username to API test endpoints that send automatic emails.');
@@ -9,6 +14,16 @@ export const MAIL_USER_PW = z
 export const MAIL_IMAP_SERVER = z
     .string()
     .describe('IMAP server of the mailbox that is used to test mail sending API endpoints.');
+export const TEST_ADMIN_EMAIL = z
+    .string()
+    .describe(
+        'Email address of the admin user. It should exist in the specified user pool and be listed under the admin emails in the environment of the API.',
+    );
+export const TEST_ADMIN_PW = z
+    .string()
+    .describe(
+        'Password of the admin user. It is necessary to make authenticated API calls with admin rights in the test suites.',
+    );
 export const TEST_USER_1_EMAIL = z
     .string()
     .describe('Email address of the first test user. It should exist in the specified user pool.');
