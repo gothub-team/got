fd5ce00ae49c16fc5d2145874e01f6d11c848214
Vincenz Koop <vincenz.koop@gmail.com>
2024-06-09 14:29:05 +0200

    used new userpool

--- packages/got-api/sst.config.ts
+++ packages/got-api/sst.config.ts
@@ -19,7 +19,6 @@ export default $config({
             home: 'aws',
             providers: {
                 aws: {
-                    // TODO: Add AWS env
                     region: env.AWS_REGION,
                     profile: env.AWS_PROFILE,
                 },
@@ -27,18 +26,13 @@ export default $config({
         };
     },
     async run() {
+        const userPool = new gotiac.UserPool('TestUserPool');
+
         const api = new gotiac.Api('TestApi', {
             domainName: env.GOT_API_DOMAIN,
-            userPoolId: env.USER_POOL_ID,
+            userPoolId: userPool.userPoolId,
             runtime: 'nodejs20.x',
             codePath: path.join(process.cwd(), 'dist/lambda/zips'),
-            // bucketNodesName: `${env.AWS_PROFILE}-nodes`,
-            // bucketEdgesName: `${env.AWS_PROFILE}-edges`,
-            // bucketReverseEdgesName: `${env.AWS_PROFILE}-reverse-edges`,
-            // bucketRightsReadName: `${env.AWS_PROFILE}-rights-read`,
-            // bucketRightsWriteName: `${env.AWS_PROFILE}-rights-write`,
-            // bucketRightsAdminName: `${env.AWS_PROFILE}-rights-admin`,
-            // bucketRightsOwnerName: `${env.AWS_PROFILE}-owners`,
         });
         return api;
     },
