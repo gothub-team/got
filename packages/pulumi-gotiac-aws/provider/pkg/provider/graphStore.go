71a5443962a799bea1a4f5198a3f474cb515d687
Vincenz Koop <vincenz.koop@gmail.com>
2024-06-12 18:04:44 +0200

    added missing export

--- packages/pulumi-gotiac-aws/provider/pkg/provider/graphStore.go
+++ packages/pulumi-gotiac-aws/provider/pkg/provider/graphStore.go
@@ -229,6 +229,7 @@ func NewGraphStore(ctx *pulumi.Context,
 	component.BucketRightsWriteName = bucketRightsWrite.Name.ToStringOutput()
 	component.BucketRightsAdminName = bucketRightsAdmin.Name.ToStringOutput()
 	component.BucketRightsOwnerName = bucketRightsOwner.Name.ToStringOutput()
+	component.BucketMediaName = bucketMedia.Name.ToStringOutput()
 
 	component.StorageReadPolicyArn = storageReadPolicy.Arn
 	component.StorageWritePolicyArn = storageWritePolicy.Arn
