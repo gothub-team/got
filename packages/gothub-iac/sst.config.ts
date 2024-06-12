8db3e65bc6f49a789835b7eae9ad997832c446ba
Vincenz Koop <vincenz.koop@gmail.com>
2024-06-12 18:06:43 +0200

    removed old filehosting

--- packages/gothub-iac/sst.config.ts
+++ packages/gothub-iac/sst.config.ts
@@ -1,23 +1,13 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 import * as gotiac from '@gothub/pulumi-gotiac-aws';
 import * as fs from 'fs';
-import {
-    AWS_MAIL_REGION,
-    AWS_PROFILE,
-    AWS_REGION,
-    FILE_HOSTING_DOMAIN,
-    MAIL_DOMAIN,
-    parseEnv,
-} from '@gothub/typescript-util';
-import { FILE_HOSTING_BUCKET } from './env';
+import { AWS_MAIL_REGION, AWS_PROFILE, AWS_REGION, MAIL_DOMAIN, parseEnv } from '@gothub/typescript-util';
 
 const env = parseEnv({
     AWS_REGION,
     AWS_PROFILE,
     AWS_MAIL_REGION,
     MAIL_DOMAIN,
-    FILE_HOSTING_DOMAIN,
-    FILE_HOSTING_BUCKET,
 });
 
 export default $config({
